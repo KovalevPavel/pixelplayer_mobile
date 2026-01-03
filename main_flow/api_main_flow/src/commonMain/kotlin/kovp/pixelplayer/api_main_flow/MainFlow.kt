2205ec134ff7bb5d137f3679d3cd9dd379e9e1e6
Павел Ковалев <inere8@gmail.com>
2026-01-03 21:13:13 +0300

    Horizontal pager in main flow + init composables for features

--- main_flow/api_main_flow/src/commonMain/kotlin/kovp/pixelplayer/api_main_flow/MainFlow.kt
+++ main_flow/api_main_flow/src/commonMain/kotlin/kovp/pixelplayer/api_main_flow/MainFlow.kt
@@ -1,44 +1,40 @@
 package kovp.pixelplayer.api_main_flow
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 import kovp.pixelplayer.feature_main_flow.MainFlowScreen
+import kovp.pixelplayer.feature_main_flow.ui.AlbumsComposable
+import kovp.pixelplayer.feature_main_flow.ui.ArtistsComposable
+import kovp.pixelplayer.feature_main_flow.ui.TracksComposable
 
 @Serializable
 object MainFlow
 
 fun NavGraphBuilder.registerMainFlow() {
     composable<MainFlow> {
-        val mainFlowNavController = rememberNavController()
-
-        NavHost(
+        HorizontalPager(
             modifier = Modifier.fillMaxSize(),
-            navController = mainFlowNavController,
-            startDestination = MainFlowScreen.entries.first().name,
+            state = rememberPagerState(pageCount = { MainFlowScreen.entries.size }),
+            key = { MainFlowScreen.entries[it] },
         ) {
-            MainFlowScreen.entries
-                .forEach { e ->
-                    composable(e.name) {
-                        when (e) {
-                            MainFlowScreen.Artists -> {
-
-                            }
-
-                            MainFlowScreen.Albums -> {
-
-                            }
-
-                            MainFlowScreen.Tracks -> {
-
-                            }
-                        }
-                    }
+            when (MainFlowScreen.entries[it]) {
+                MainFlowScreen.Artists -> {
+                    ArtistsComposable()
                 }
+
+                MainFlowScreen.Albums -> {
+                    AlbumsComposable()
+                }
+
+                MainFlowScreen.Tracks -> {
+                    TracksComposable()
+                }
+            }
         }
     }
 }
---
+++ main_flow/feature_main_flow/src/commonMain/kotlin/kovp/pixelplayer/feature_main_flow/ui/AlbumsComposable.kt
@@ -0,0 +1,44 @@
+package kovp.pixelplayer.feature_main_flow.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kovp.pixelplayer.core_design.AppPreview
+import kovp.pixelplayer.core_design.AppTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AlbumsComposable(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(title = { Text(text = "Albums") })
+        },
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(paddingValues = paddingValues),
+            contentPadding = PaddingValues(bottom = 32.dp),
+        ) {
+
+        }
+    }
+}
+
+@AppPreview
+@Composable
+private fun AlbumsPreview() {
+    AppTheme {
+        AlbumsComposable()
+    }
+}
