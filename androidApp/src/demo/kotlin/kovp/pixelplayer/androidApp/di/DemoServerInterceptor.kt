a42c8d78a898c527a3203b6ca3a9584b0ca8bdd0
Павел Ковалев <inere8@gmail.com>
2026-04-28 13:29:51 +0300

    Added https to demo server

---
+++ androidApp/src/demo/kotlin/kovp/pixelplayer/androidApp/di/DemoServerInterceptor.kt
@@ -0,0 +1,22 @@
+package kovp.pixelplayer.androidApp.di
+
+import android.content.Context
+import kotlinx.io.IOException
+import kovp.pixelplayer.R
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class DemoServerInterceptor(
+    private val context: Context,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val demoServerIp = context.getString(R.string.demo_server_ip)
+
+        val originalRequest = chain.request()
+
+        return when (originalRequest.url.host) {
+            demoServerIp -> chain.proceed(originalRequest)
+            else -> throw IOException("not demo app url")
+        }
+    }
+}
